86c4638b1aadf18db8102a5153441d9ebafddb3f
Sebastien Varrette <sebastien.varrette@ssi.gouv.fr>
2025-11-15 13:17:55 +0100

    feat(yasnippet): complete sh template
    Signed-off-by: Sebastien Varrette <sebastien.varrette@ssi.gouv.fr>

--- templates/yatemplates/00:.*.sh
+++ templates/yatemplates/00:.*.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-# Time-stamp: <>
+# Time-stamp: <Sat 2025-11-15 13:13 svarrette>
 ################################################################################
 # ${1:$$(replace-regexp-in-string "_" " " (file-name-nondirectory (file-name-sans-extension(buffer-file-name))))} - $0
 #
@@ -18,23 +18,25 @@ COLOR_RESET="\033[0m"
 #SCRIPTDIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #TOP_DIR="$(  cd "$( dirname "${BASH_SOURCE[0]}" )" && git rev-parse --show-toplevel)"
 CMD_PREFIX="echo"
+FORCE=
 OPTS=
 
 ################################################################################
-# info()    { echo -e "\${COLOR_BOLD}$*\${COLOR_RESET}"; }
-# error()   { echo -e "\${COLOR_BOLD}*** ERROR *** $*\${COLOR_RESET}"; }
-# warning() { echo -e "\${COLOR_VIOLET}/!\\ WARNING:\${COLOR_RESET} $*"; }
-warning() { echo -e "/!\\ WARNING: $*"; }
-error() { echo -e "*** ERROR *** $*"; }
+info()    { echo -e "\${COLOR_BOLD}$*\${COLOR_RESET}"; }
+error()   { echo -e "\${COLOR_BOLD}*** ERROR *** $*\${COLOR_RESET}"; }
+# error() { echo -e "*** ERROR *** $*"; }
+warning() { echo -e "\${COLOR_VIOLET}/!\\ WARNING:\${COLOR_RESET} $*"; }
+# warning() { echo -e "/!\\ WARNING: $*"; }
 print_error_and_exit() { error "$*"; exit 1; }
 usage() {
   cat <<EOF
 $(basename "\$0"):
 
 USAGE
-  \$0 [-x]
+  $(basename "\$0") [-f] [-x]
 
 OPTIONS:
+  -f --force      Force mode (don't ask anything)
   -n --dry-run    Dry run mode (**DEFAULT** mode): echo the commands to be run
   -x --exec       Really execute the commands, i.e. don't just echo them
 
@@ -55,16 +57,26 @@ warning_dry_run() {
     exit 0
   fi
 }
+###
+# Ask for a question - return 0 (true) if Yes is answered (default value)
+##
+ask(){
+  [ -n "\${FORCE}" ] && return 0 # aka don't ask...
+  echo -n -e "\${COLOR_VIOLET}/!\ WARNING:\${COLOR_RESET} $* [Y|n] "
+  read                                          #   0: true     1:false
+  [[ -z "$REPLY" || $REPLY =~ ^[Yy].*$ ]] && return 0 || return 1
+}
+# As above, but return 0 (true) if No is answered
+avoid(){
+  [ -n "\${FORCE}" ] && return 0 # aka don't ask...
+  echo -n -e "\${COLOR_VIOLET}/!\ WARNING:\${COLOR_RESET} $* [y|N] "
+  read                                          #   0: true     1:false
+  [[ -z "$REPLY" || $REPLY =~ ^[Nn].*$ ]] && return 0 || return 1
+}
 ####
 # ask to continue. exit 1 if the answer is no
 ##
-really_continue() {
-  echo -e -n "[\${COLOR_VIOLET}/!\\ WARNING:\${COLOR_RESET}] Are you sure you want to continue? [Y|n] "
-  read -r ans
-  case $ans in
-    n* | N*) exit 1 ;;
-  esac
-}
+really_continue() { ask "Are you sure you want to continue?" || exit 1; }
 
 ################################################################################
 #[ $UID -gt 0 ] && print_error_and_exit "You must be root to execute this script (current uid: $UID)"
@@ -81,13 +93,17 @@ while [ $# -ge 1 ]; do
 done
 
 # Safeguards
+# check availability of the used commands
+# for cmd in python3; do
+#   [ ! -x "$(command -v \${cmd} 2>/dev/null)" ] && print_error_and_exit "Unable to find the command \${cmd}. Exiting"
+# done
 # [...]
 
 # Let's go
 CMD="whoami"
 # [...]
 CMD+=" \${OPTS}" # don't forget ' '
-# Alternative usage: just prefix all your commands with \{CMD_PREFIX}
+# Alternative usage: just prefix all your commands with \{CMD_PREFIX}. Ex: \{CMD_PREFIX} whoami
 #
 #################################################
 warning "about to execute the following command:"
